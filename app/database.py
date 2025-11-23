5730c1619a3829ae982343b15060a2e6884a2bff
uanmita <uanmita@gmail.com>
2025-11-23 13:46:26 +0100

    Refactor: seed DB refactor to create tables before seed data

--- app/database.py
+++ app/database.py
@@ -1,8 +1,17 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./fichajes.db"
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+# Default to SQLite for local development if DATABASE_URL is not set
+SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL", "sqlite:///./fichajes.db")
+
+# Render's PostgreSQL URLs start with 'postgres://', but SQLAlchemy expects 'postgresql://'
+if SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
+    SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
+# For SQLite, we need to allow multithreading
+connect_args = {"check_same_thread": False} if SQLALCHEMY_DATABASE_URL.startswith("sqlite") else {}
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args=connect_args)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
-
