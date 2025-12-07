ed105055a8f3af07a92453d4f44b25a95cc6bea1
uanmita <uanmita@gmail.com>
2025-12-07 19:48:52 +0000

    feat: Refactor improve SSL conecction for secutiry

--- app/database.py
+++ app/database.py
@@ -1,17 +1,31 @@
 import os
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy import create_enginerom sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-# Default to SQLite for local development if DATABASE_URL is not set
+# --- DATABASE URL CONFIGURATION ---
 SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL", "sqlite:///./fichajes.db")
 
-# Render's PostgreSQL URLs start with 'postgres://', but SQLAlchemy expects 'postgresql://'
+# FIX for Render's PostgreSQL URL format
 if SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
     SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql://", 1)
+# ---------------------------------
 
-# For SQLite, we need to allow multithreading
-connect_args = {"check_same_thread": False} if SQLALCHEMY_DATABASE_URL.startswith("sqlite") else {}
+# --- ENGINE CONFIGURATION ---
+# Default arguments
+engine_args = {}
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args=connect_args)
+# If SQLite, add specific connect_args for single-threaded operation
+if SQLALCHEMY_DATABASE_URL.startswith("sqlite"):
+    engine_args['connect_args'] = {"check_same_thread": False}
+
+# If PostgreSQL, add specific connect_args to require SSL (for Render)
+elif SQLALCHEMY_DATABASE_URL.startswith("postgresql"):
+    engine_args['connect_args'] = {"sslmode": "require"}
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, **engine_args
+)
+# ---------------------------
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
